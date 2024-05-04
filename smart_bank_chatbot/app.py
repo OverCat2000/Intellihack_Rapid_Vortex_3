fb056cc81368d6baaf7549858b24c13374fe0273
JanithRavinduRashmika <rashmikajanithravindu@gmail.com>
2024-05-04 12:47:50 +0530

    Task 3 code completed.

---
+++ smart_bank_chatbot/app.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+if "vector" not in st.session_state:
+    st.session_state.embeddings = OpenAIEmbeddings()
+    st.session_state.loader = PyPDFLoader('LLM Scenario.pdf')
+    st.session_state.docs = st.session_state.loader.load()[1:]
+    st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+    st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs)
+    st.session_state.vector = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+
+st.title("Smart Bank Chatbot")
+
+llm = ChatOpenAI(model= "gpt-3.5-turbo")
+
+retriever = st.session_state.vector.as_retriever()
+
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+qa_system_prompt = """You are a bank assistant for question-answering tasks. \
+Use the following pieces of retrieved context and general banking knowlage to answer the question. \
+Give output in tone of friendly bank assistant.
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+st.session_state.store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in st.session_state:
+        st.session_state[session_id] = ChatMessageHistory()
+    return st.session_state[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+input_prompt = st.chat_input("Enter your question.")
+
+if input_prompt:
+
+    with st.chat_message("user"):
+        st.markdown(input_prompt)
+
+    st.session_state.messages.append({"role":"user","content":input_prompt})
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = conversational_rag_chain.invoke(
+                            {"input": input_prompt},
+                            config={"configurable": {"session_id": "abc123"}},
+                        )["answer"]
+
+        message_placeholder.markdown(full_response)
+    
+    st.session_state.messages.append({"role":"assistant", "content":full_response})
+
+    
